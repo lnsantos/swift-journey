9ce3f0c5147ffc9abd39b4113bdf86bf1049147e
Lucas <lucas.nepomuceno1999@gmail.com>
2023-10-16 21:43:40 -0300

    new: create simple variable

--- the_basics.swift
+++ the_basics.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+var variableNumber = 42
+var variabelString = "Learning"
+var variableDouble = 0.6
+
+let myConstant = 42
+
+print("const is ", myConstant)
+print("number is ", variableNumber)
+print("double is ", variableDouble)
+print("string is ", variabelString)
