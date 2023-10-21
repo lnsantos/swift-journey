167b3972d552c695f64297ffe1a5982e1c32c2c7
Lucas <lucas.nepomuceno1999@gmail.com>
2023-10-21 15:51:11 -0300

    new: Explored enum in swift

--- enumerations_and_structures.swift
+++ enumerations_and_structures.swift
@@ -34,3 +34,42 @@ enum Barcode {
     case upc(Int, Int, Int, Int)
     case qrCode(String)
 }
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+
+switch productBarcode {
+    case let .upc(numberSystem, manufacturer, product, check):
+        print("UPC : \(numberSystem), \(manufacturer), \(product), \(check).")
+    case let .qrCode(productCode):
+        print("QR code: \(productCode).")
+}
+
+productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
+
+switch productBarcode {
+    case .upc(let numberSystem, let manufacturer, let product, let check):
+        print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+    case .qrCode(let productCode):
+        print("QR code: \(productCode).")
+}
+
+// Raw value
+enum EnumRawString : String, CaseIterable {
+    case Failed = "Failed your payment"
+    case Success = "Success in payment"
+}
+
+for i in EnumRawString.allCases {
+    print(i.rawValue)
+}
+
+enum EnumRawNative : String, CaseIterable {
+    case About, Home
+}
+
+for i in EnumRawNative.allCases {
+    print(i.rawValue)
+}
+
+let testInitRaw = EnumRawNative(rawValue: "About")
+print(testInitRaw?.rawValue)
