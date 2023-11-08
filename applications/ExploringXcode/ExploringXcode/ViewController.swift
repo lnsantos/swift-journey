8cb4263dd7d363aae5414dbd3a1cfb1a39fc126b
Lucas <lucas.nepomuceno1999@gmail.com>
2023-11-08 18:27:41 -0300

    added first example to test in IDE

---
+++ applications/ExploringXcode/ExploringXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExploringXcode
+//
+//  Created by Lucas Santos on 01/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
